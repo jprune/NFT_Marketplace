163faa3e4cfa58bb66ef14a7f4e7fb3b08d7c391
Julian <105754345+jprune@users.noreply.github.com>
2022-08-13 17:42:11 +0200

    finished UI

--- screens/Home.js
+++ screens/Home.js
@@ -1,23 +1,37 @@
 import { useState } from "react"
-import { Text, View, SafeAreaView, FlatList } from "react-native"
+import { View, SafeAreaView, FlatList } from "react-native"
 
 import { COLORS, NFTData } from "../constants"
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components"
 
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData)
+
+  const handleSearch = (value) => {
+    if(!value.length) return setNftData(NFTData)
+
+    const filteredData = NFTData.filter((item) => item.name.toLowerCase().includes(value.toLowerCase()))
+
+    if(filteredData.length) {
+      setNftData(filteredData)
+    } else {
+      setNftData(NFTData)
+    }
+  }
+
   return (
     <SafeAreaView style={{ flex: 1}}>
-      <FocusedStatusBar barStyle={"dark-content"} showHideTransition={"none"}/>
+      <FocusedStatusBar barStyle={"dark-content"} translucent={true} showHideTransition={"none"}/>
 
       <View style={{ flex: 1}}>
         <View style={{ zIndex: 0}}>
           <FlatList 
-            data={NFTData}
+            data={nftData}
             renderItem={({ item }) => <NFTCard data={item} />}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
           />
         </View>
 
